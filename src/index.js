9acd66a32e59c46437f97c666b8b0b72e72eece5
Bruno <bruno.carv@icloud.com>
2019-10-17 00:27:48 -0300

    gitUser

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import './config/reactotronConfig';
+import Routes from './routes';
+
+const App = () => (
+  <>
+    <StatusBar barStyle="ligh-content" backgroundColor="#7159c1" />
+    <Routes />
+  </>
+);
+
+export default App;
